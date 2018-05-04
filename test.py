0dcf0a1e73dd05a6365ee986d0c15932013e8460
d.linosilva <d.linosilva@gmail.com>
2018-05-04 11:56:49 -0400

    Update index

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Rodrigo Gordo! "Você é complexado")
+        self.assertRegex(result.data.decode(), "Rodrigo Gordo! "Voce e complexado")
 
 
 if __name__ == "__main__":
